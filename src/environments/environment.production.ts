c7b44a9964159b6b00b535693efb9cf1e370d500
DANIEL CHIPOLESCH <danielchipolesch@gmail.com>
2024-06-20 21:28:11 -0300

    Correção de erro no nome da variável

--- src/environments/environment.production.ts
+++ src/environments/environment.production.ts
@@ -1,3 +1,3 @@
 export const environment = {
-    todoListApi: 'todolistapi2-env.eba-a2wqmav5.us-east-2.elasticbeanstalk.com/api/todos'
+    todolistApi: 'todolistapi2-env.eba-a2wqmav5.us-east-2.elasticbeanstalk.com/api/todos'
 };
